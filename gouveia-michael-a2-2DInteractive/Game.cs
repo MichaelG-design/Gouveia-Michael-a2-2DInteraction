a7c9dc0f4cf2525ac3404c2ab3c7a8f6485185cc
Michael Gouveia <mtgouveia06@gmail.com>
2025-01-28 14:29:42 -0500

    So far not too bad

--- gouveia-michael-a2-2DInteractive/Game.cs
+++ gouveia-michael-a2-2DInteractive/Game.cs
@@ -3,31 +3,33 @@ using System;
 using System.Numerics;
 
 // The namespace your code is in.
-namespace MohawkGame2D
+namespace MohawkGame2D;
+
+///     Your game code goes inside this class!
+public class Game
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
-    public class Game
+    ///     Setup runs once before the game loop begins.
+    public void Setup()
     {
-        // Place your variables here:
-
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
-        public void Setup()
-        {
-
-        }
-
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
-        public void Update()
-        {
-
-        }
+        Window.SetTitle("2D Interactive Drawing"); //Name of window
+        Window.SetSize(400, 400); //Set window size
     }
 
+    public void Update()
+    {
+        Window.ClearBackground(Color.Green); //Grass background
+
+        Draw.FillColor = Color.Gray; //The road
+        Draw.Rectangle(0, 160, 400, 100);
+        
+        Draw.FillColor = Color.Blue; //The blue car, user can move it up and down
+        Draw.Rectangle(200, Input.GetMouseY(), 100, 50);
+        Draw.LineColor = Color.Blue;
+        Draw.Rectangle(220, Input.GetMouseY(), 60, 80);
+
+        Draw.FillColor = Color.Black; //Blue car wheels
+        Draw.Circle(220, Input.GetMouseY(), 15);
+        Draw.FillColor = Color.Black;
+        Draw.Circle(280, Input.GetMouseY(), 15);
+    }
 }
